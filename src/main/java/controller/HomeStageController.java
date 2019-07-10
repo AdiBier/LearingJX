cb4aacfa0c673da7afcf87c0e87f2b06fee74e39
AdiBier <AdiBier@interia.eu>
2019-07-10 21:57:14 +0100

    GOOD

--- src/main/java/controller/HomeStageController.java
+++ src/main/java/controller/HomeStageController.java
@@ -2,10 +2,18 @@ package controller;
 
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,6 +21,9 @@ public class HomeStageController implements Initializable {
 
     @FXML
     private JFXHamburger ham1;
+
+    @FXML
+    private Button buttonGoLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -23,6 +34,14 @@ public class HomeStageController implements Initializable {
             basicClose.play();
         });
 
+    }
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+        Parent loginStageParent = FXMLLoader.load(getClass().getResource("/LoginStage.fxml"));
+        Scene loginStageScene = new Scene(loginStageParent);
 
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(loginStageScene);
+        window.show();
     }
 }
