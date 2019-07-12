3f8ed6d5f922367a7f2b4a42a4fc312e5bce44e6
AdiBier <AdiBier@interia.eu>
2019-07-12 13:44:06 +0100

    MoreStaffmore

---
+++ src/main/java/service/OneClickButtonService.java
@@ -0,0 +1,10 @@
+package service;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public interface OneClickButtonService {
+
+    void changeScreenButtonPushed(ActionEvent event) throws IOException;
+}
--- src/main/java/service/TwoClickButtonService.java
+++ src/main/java/service/TwoClickButtonService.java
@@ -4,7 +4,8 @@ import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
-public interface ButtonService {
+public interface TwoClickButtonService {
 
     void changeScreenButtonPushed(ActionEvent event) throws IOException;
+    void changeScreenButtonPushed2(ActionEvent event) throws IOException;
 }
